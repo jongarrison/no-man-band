dfdcd4c6ff23f5cc6cf974cdf0420d04263f1e34
Jon Garrison <jonathan.garrison@nike.com>
2021-02-12 12:28:33 -0800

    Cleaning it up and making the repl easier to use

--- noManBand.js
+++ noManBand.js
@@ -1,101 +1,156 @@
+const { conforms } = require("lodash");
+
 (function(noManBand) {
 
-  var midiHelper = require('./lib/midiHelper');
-  var noteHelper = require('./lib/noteHelper');
-  var _ = require('lodash');
+  const repl = require("repl");  
+  const vm = require('vm'); //see: https://nodejs.org/api/vm.html#vm_script_runincontext_contextifiedobject_options
+  const midiHelper = require('./lib/midiHelper');
+  const noteHelper = require('./lib/noteHelper');
+  const util = require('util');
+  const _ = require('lodash');
   
-  var interval;
-  var noteDuration = 1000;
-  var cycle = 0;
+  let replLog;
+  let musicTimingInterval;
+  let noteDuration = 1000;
+  let cycle = 0;
 
   console.log("About to start REPL");
-  var repl = require("repl");
-  
-  var util = require('util');
-  var replServer = repl.start({
+  const replServer = repl.start({
     prompt: "noManBand> ",
-    breakEvalOnSigint: true,
+    breakEvalOnSigint: false, //Can't be true if "eval" is overriden
+    useGlobal: false, //Can't be true if "eval" is overriden
+    terminal: true,
     ignoreUndefined: true,
-    useGlobal: true,
-    useColors: true
+    useColors: true,
+    eval: (cmd, evalContext, filename, callback) => {
+      try {
+        const script = new vm.Script(cmd);
+        let result = script.runInContext(evalContext);
+
+        Promise.resolve(result)
+            .then(async () => {
+
+              if (!_.isEmpty(result) && result.hasOwnProperty("then")) {
+                await result;
+              }
+
+              callback(null, result);
+              replServer.displayPrompt();
+            })
+            .catch((error) => {
+              console.error("Error while evaluating repl:");
+              console.error(error);
+              callback(error);
+              replServer.displayPrompt();
+            });
+
+      } catch (e) {
+        console.error("Uncaught error:");
+        console.error(e);
+        callback(e);
+      }
+    },
   });
-  
-  var context = replServer.context;
+
+  const context = replServer.context;
+  context.console.log = (msg) => {
+    //This approach is based on: https://stackoverflow.com/questions/28683743/adjust-node-repl-to-not-clobber-prompt-on-async-callback/63121427#63121427
+    const promptOffset = replServer._prompt.length + replServer.line.length;
+    replServer.output.write('\033[2K\033[1G'); // Erase to beginning of line, and reposition cursor at beginning of line
+    replServer.output.write(msg + "\n");
+    replServer.output.write('' + replServer._prompt + replServer.line); // Redraw existing line
+    replServer.output.write('\033[' + (promptOffset + 1) + 'G'); // Move the cursor to where it was
+
+    //This needs to be called with: context.console.log or the replLog shortcut
+  };
+  replLog = context.console.log;
+
   context.require = require;
   context.module = module;
-  context.context = context;
+  //context.context = context;
   context.connect = midiHelper.connect;
   context.listOutputPorts = midiHelper.listOutputPorts;
+  context.list = midiHelper.listOutputPorts;
   context.connectVirtualOutput = midiHelper.connectVirtualOutput;
   
-  context.BPM = 180;
-  context.playImpromptuOn = true;
-  context.playScaleOn = false;
-  context.playBeatOn = false;
-  context.impromptuInputs = [
-    ["A", "B", "C", ""],
-    ["C", "C", "A", ""]
-  ];
-  context.impromptuInputsCycle = [
-    0, 0, 0, 1
-  ];
-  context.impromptuOctive = 3;
+  const conf = {
+    BPM: 180,
+    playImpromptuOn: true,
+    playScaleOn: false,
+    playBeatOn: false,
+    impromptuInputs: [
+      ["A", "B", "C", ""],
+      ["C", "C", "A", ""]
+    ],
+    impromptuInputsCycle: [
+      0, 0, 0, 1
+    ],
+    impromptuOctive: 3,
+  };
+  context.conf = conf;
+
+  //Add functions for toggling the active musical approach
+  context.i = () => {
+    conf.playImpromptuOn = !conf.playImpromptuOn;
+  };
+  context.b = () => {
+    conf.playBeatOn = !conf.playBeatOn;
+  };
+  context.s = () => {
+    conf.playScaleOn = !conf.playScaleOn;
+  };
   
   context.start = () => {
-    console.log("Start!");
-    var cycleInterval = 1000/(context.BPM/60);
-    console.log("cycle interval: ", cycleInterval);
+    replLog("Start!");
+    const cycleInterval = 1000/(conf.BPM/60);
+    replLog("cycle interval: ", cycleInterval);
     
-    interval = setInterval(context.playCycle, cycleInterval);
+    musicTimingInterval = setInterval(context.playCycle, cycleInterval);
   };
   
   context.playCycle =() => {
-    cycle++;
-    //console.log("PLAY CYCLE: ", cycle);
-    
-    //these are the steps of the program
-  
-    if (context.playScaleOn) context.playScale(cycle);
-    if (context.playBeatOn) context.playBeat(cycle);
-    if (context.playImpromptuOn) context.playImpromptu(cycle);
+    cycle++;      
+    if (conf.playScaleOn) context.playScale(cycle);
+    if (conf.playBeatOn) context.playBeat(cycle);
+    if (conf.playImpromptuOn) context.playImpromptu(cycle);
   }
   
   context.stop = () => {
-    console.log("Stop!");
-    clearInterval(interval);
+    replLog("Stop!");
+    clearInterval(musicTimingInterval);
   };
   
   context.playScale = (noteIndexStart) => {
-    var octave = 4;
+    const octave = 4;
     if (typeof noteIndexStart === 'undefined') noteIndexStart = 0;
     
     noteIndexStart = noteIndexStart % noteHelper.scale.length;
-    var noteName = noteHelper.scale[noteIndexStart];
-    var noteNumber = noteHelper.notes[noteName] + (octave * 12);
-    console.log("play note: ", noteName, " from noteIndexStart: ", noteIndexStart);
+    const noteName = noteHelper.scale[noteIndexStart];
+    const noteNumber = noteHelper.notes[noteName] + (octave * 12);
+    replLog(`play note=${noteName} from noteIndexStart ${noteIndexStart}`);
   
     midiHelper.sendMessage([144, noteNumber, 100]);
   };
   
   context.playBeat = (cycle) => {
-    var cyclePosition = cycle % 4; //0 based
+    const cyclePosition = cycle % 4; //0 based
     
     const lowOctave = 5;
     const highOctave = 6;
   
-    var noteNumber;
+    let noteNumber;
     if (cyclePosition === 2) {
       noteNumber = 24 + (lowOctave * 12);
       midiHelper.sendMessage([144, noteNumber, 100]);
   
-      console.log("beat cyclePosition:", cyclePosition, " lowOctave");
+      replLog(`beat cyclePosition=${cyclePosition} lowOctave`);
     } else if (cyclePosition === 3) {
       noteNumber = 24 + (highOctave * 12);
       midiHelper.sendMessage([144, noteNumber, 100]);
   
-      console.log("beat cyclePosition:", cyclePosition, " highOctave");
+      replLog(`beat cyclePosition=${cyclePosition} highOctave`);
     } else {
-      console.log("beat cyclePosition:", cyclePosition, " quiet");
+      replLog(`beat cyclePosition=${cyclePosition} quiet`);
     }
     // setTimeout(function() {
     //   midiHelper.sendMessage([128, noteNumber, 100]);
@@ -103,40 +158,43 @@
   };
   
   context.flattenImpromptu = () => {
-    var result = [];
-    _.each(context.impromptuInputsCycle, function(impromptuInput) {
-      result = _.concat(result, context.impromptuInputs[impromptuInput]);
+    //replLog(`flattenImpromptu`);
+    let result = [];
+    _.each(conf.impromptuInputsCycle, function(impromptuInput) {
+      result = _.concat(result, conf.impromptuInputs[impromptuInput]);
     });
     return result;
   };
   
   context.playImpromptu = (cycle) => {
-    var inputs = flattenImpromptu();
+    //replLog(`playImpromptu`);
+    const inputs = context.flattenImpromptu();
     
-    var cyclePosition = cycle % inputs.length; //0 based
-    var cycleNoteName = inputs[cyclePosition];
+    const cyclePosition = cycle % inputs.length; //0 based
+    const cycleNoteName = inputs[cyclePosition];
     
-    var noteNumber = noteHelper.notes[cycleNoteName];
+    let noteNumber = noteHelper.notes[cycleNoteName];
     
     if (noteNumber) {
-      noteNumber = noteNumber + (context.impromptuOctive * 12);
-      console.log("impromptu cyclePosition: ", cyclePosition, " cycleNoteName: ", cycleNoteName);
+      noteNumber = noteNumber + (conf.impromptuOctive * 12);
+      replLog(`impromptu cyclePosition=${cyclePosition} cycleNoteName=${cycleNoteName}`);
       midiHelper.sendMessage([144, noteNumber, 100]);
     } else {
-      console.log("impromptu cyclePosition: ", cyclePosition, " rest");
+      replLog(`impromptu cyclePosition=${cyclePosition} rest`);
     }
   };
   
-  context.playOneNoteAndQuit = () => {
+  context.playNote = () => {
+    //replLog(`playNote`);
     
-    var delay = 1000;
-    var octave = 4;
-    var noteNumber = 24 + (octave * 12);
+    const delay = 1000;
+    const octave = 4;
+    const noteNumber = 24 + (octave * 12);
     
     midiHelper.sendMessage([144, noteNumber, 100]);
     
     setTimeout(function() {
-      console.log("Playing one note");
+      replLog("Playing one note");
     }, delay);
     
     setTimeout(function() {
@@ -144,4 +202,8 @@
     }, delay + 200);
     
   };
+
+  //Set the user up to connect quickly...
+  context.listOutputPorts();
+  replServer.displayPrompt();
 })(module.exports);
